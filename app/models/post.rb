0dd86524de4929f33693ee45620fd4e69f4c7cc4
이강호 <cannalee90@gmail.com>
2016-11-06 14:41:54 +0900

    post indentation changed

--- app/models/post.rb
+++ app/models/post.rb
@@ -48,29 +48,29 @@ class Post < ActiveRecord::Base
     i = 1
     limit = 20
     loop do
-        break if content2[(limit * (i - 1))..((limit * i)-1)].nil?
-        arr << content2[(limit * (i - 1))..((limit * i)-1)]
-        i += 1
+      break if content2[(limit * (i - 1))..((limit * i)-1)].nil?
+      arr << content2[(limit * (i - 1))..((limit * i)-1)]
+      i += 1
     end
     arr2 = []
     arr.each do |e|
-        arr2 << e.join(' ')
+      arr2 << e.join(' ')
     end
     arr3 = []
     j = 1
     arr2.each do |f|
-        arr3 << "<span class=\"span_block\" id=\"block#{j}\">" + f + "</span>&nbsp;"
-        j += 1
+      arr3 << "<span class=\"span_block\" id=\"block#{j}\">" + f + "</span>&nbsp;"
+      j += 1
     end
 
     k = 1
     self.replies.reorder("like_count desc").each do |reply|
-        arr3.each do |block|
-            if block.include?(reply.location)
-                block.gsub!("#{reply.location_text.strip}", "<span class=\"ggum_ddak_ji #{"most_best" if k == 1}\" id=\"#{reply.id}\">#{reply.location_text}</span>")
-            end
+      arr3.each do |block|
+        if block.include?(reply.location)
+          block.gsub!("#{reply.location_text.strip}", "<span class=\"ggum_ddak_ji #{"most_best" if k == 1}\" id=\"#{reply.id}\">#{reply.location_text}</span>")
         end
-        k += 1
+      end
+      k += 1
     end
     content3 = arr3.join
     return content3
